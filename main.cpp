9735416ed071f7e3b22d8c98c14c3ac17dafcddc
max <max.koeg@gmail.com>
2026-09-23 00:03:02 -0700

    touchpoint 1

--- main.cpp
+++ main.cpp
@@ -1,1 +1,40 @@
 //maxwell koegler | COMSC 210 | lab 15 | 9/22/26
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Movie {
+private:
+string screenWriter;
+int yearReleased;
+string title;
+public:
+
+void setScreenWriter(string w) {
+    screenWriter = w;
+}
+string getScreenWriter() {
+    return screenWriter
+}
+
+void setYearReleased(int y) {
+    yearReleased = y;
+}
+int getYearReleased(){
+    return yearReleased;
+}
+
+void setTitle(string w) {
+    title = w;
+}
+string getTitle() {
+    return title;
+}
+
+}
+
+int main(){
+
+}
