64b1aae21ef557df505abf5eb8f20a17513f758f
max <max.koeg@gmail.com>
2026-09-23 00:33:29 -0700

    final commit

--- main.cpp
+++ main.cpp
@@ -1,73 +1,58 @@
 //maxwell koegler | COMSC 210 | lab 15 | 9/22/26
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
 
 using namespace std;
-
-class Movie {
+class Movie { //movie class with protected vars and appropriate setters and getters
 private:
 string screenWriter;
 int yearReleased;
 string title;
 public:
-
 void setScreenWriter(string w) {
     screenWriter = w;
 }
 string getScreenWriter() {
     return screenWriter;
 }
-
 void setYearReleased(int y) {
     yearReleased = y;
 }
 int getYearReleased(){
     return yearReleased;
 }
-
 void setTitle(string w) {
     title = w;
 }
 string getTitle() {
     return title;
 }
-
-void print() {
+void print() { //print helper function
     cout << "Screen Writer: " << screenWriter << endl;
     cout << "Year released : " << yearReleased << endl;
     cout << "Title: " << title << endl;
 }
-
 };
 
 int main(){
-    ifstream file("input.txt");
-
+    ifstream file("input.txt"); //input file init
     string line1;
     int line2;
     string line3;
-
-    vector<Movie> movies;
-
+    vector<Movie> movies; //vector storage of type movies
     while(getline(file, line1)) {
-        file >> line2;
+        file >> line2; //line reading
         file.ignore();
-        
-        Movie temp = Movie();
+        getline(file, line3);  
+        Movie temp = Movie(); //temp init
         temp.setTitle(line1);
         temp.setYearReleased(line2);
         temp.setScreenWriter(line3);
-
-        movies.push_back(temp);
+        movies.push_back(temp); //temp push to container
     }
-
     for(Movie m : movies){
-        cout << "    Movie: " << m.getTitle() << endl;
-        cout << "Year Released: " << m.getYearReleased() << endl;
-        cout << "Screenwriter: " << m.getScreenWriter() << endl;
+        m.print();
     }
-
 }
